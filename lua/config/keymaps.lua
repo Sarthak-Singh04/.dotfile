203fdcdd257de53cb7f371e94f6e73b9151a96ef
Sarthak-Singh04 <singhsarthak691@gmail.com>
2024-07-06 16:36:50 +0530

    add a open and close terminal functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,8 +10,9 @@ discipline.cowboy()
 local keymaps = vim.keymap
 local opts = { noremap = true, silent = true }
 
---undo key
-keymaps.set("i", "<leader>u", "<cmd>undo<cr>")
+-- Undo key
+keymaps.set("n", "<C-z>", "u", opts)
+keymaps.set("i", "<C-z>", "<C-o>u", opts)
 
 -- Increment/decrement
 keymaps.set("n", "+", "<C-a>")
@@ -32,15 +33,47 @@ keymaps.set("n", "<tab>", ":tabnext<Return>", opts)
 keymaps.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
-keymaps.set("n", "ss", ":split<Return>", opts)
-keymaps.set("n", "sk", "<C-w>k")
-keymaps.set("n", "sj", "<C-w>j")
-keymaps.set("n", "sl", "<C-w>l")
+keymaps.set("n", "<leader>h", ":split<Return>", opts)
+keymaps.set("n", "<leader>v", ":vsplit<Return>", opts)
+
+-- Close current split
+keymaps.set("n", "<leader>c", "<C-w>c", opts)
+
+-- Close horizontal split
+keymaps.set("n", "<leader>ch", function()
+  local layout = vim.fn.winlayout()
+  if layout[1] == "row" then
+    vim.cmd("wincmd c")
+  end
+end, opts)
+
+-- Close vertical split
+keymaps.set("n", "<leader>cv", function()
+  local layout = vim.fn.winlayout()
+  if layout[1] == "col" then
+    vim.cmd("wincmd c")
+  end
+end, opts)
+
+-- Move between split windows
+keymaps.set("n", "<A-Left>", "<C-w>h", opts)
+keymaps.set("n", "<A-Up>", "<C-w>k", opts)
+keymaps.set("n", "<A-Down>", "<C-w>j", opts)
+keymaps.set("n", "<A-Right>", "<C-w>l", opts)
 
 -- Resize window
 keymaps.set("n", "<C-w><left>", "<C-w>>")
 keymaps.set("n", "<C-w><right>", "<C-w><")
+keymaps.set("n", "<C-w><up>", "<C-w>+")
 keymaps.set("n", "<C-w><down>", "<C-w>-")
+
+-- Open terminal in horizontal split
+keymaps.set("n", "<leader>th", ":split | terminal<Return>", opts)
+-- Open terminal in vertical split
+keymaps.set("n", "<leader>tv", ":vsplit | terminal<Return>", opts)
+
+-- Close terminal
+keymaps.set("t", "<Esc>", [[<C-\><C-n>:q<CR>]], opts)
 
 -- Diagnostic
 keymaps.set("n", "<C-j>", function()
